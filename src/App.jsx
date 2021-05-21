ebe3dbb786b59a6488d8fb5df630397dab17f785
unknown <r.loboplaza@hotmail.com>
2021-05-21 16:18:14 -0300

    configurando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola react</h1>
+    </div>
+  );
+};
+
+export default App;
