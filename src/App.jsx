21e3edf2e89062f47a398853ec4e858875efc562
unknown <r.loboplaza@hotmail.com>
2021-05-22 11:11:17 -0300

    agregando header

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,34 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+// import Home from './components/Home';
+import Particles from 'react-particles-js';
+import Header from './components/Header';
+import './css/app.css';
 
 const App = () => {
   return (
     <div>
       <Router>
-        <Navbar>
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
-        </Navbar>
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 30,
+                density: {
+                  enable: true,
+                  value_area: 900,
+                },
+              },
+            },
+            color: {
+              Value: '#f00',
+            },
+          }}
+        />
+        <Navbar />
+        <Switch>{/* <Route exact path="/" component={Home} /> */}</Switch>
+        <Header />
       </Router>
     </div>
   );
