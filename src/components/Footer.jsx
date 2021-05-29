46a62937cbff97b2bf7a3167dbbf3b56ce29e0d4
unknown <r.loboplaza@hotmail.com>
2021-05-29 11:21:17 -0300

    agregando estilos a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,63 @@
 import React from 'react';
 import '../css/footer.css';
-
+import { Link } from 'react-router-dom';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
   return (
     <>
-      <div className="footer">
-        <div className="footer-container">
+      <div className="footer-container">
+        <div className="footer-section">
           <div className="footer-left">
             <div className="links">
               <ul>
-                <li>Github</li>
-                <li>Instagram</li>
-                <li>Linkedin</li>
+                <li>
+                  <a href="http://github.com/rafalobop">
+                    <FaGithub />
+                  </a>
+                </li>
+                <li>
+                  <a href="http://instagram.com/rafalobop">
+                    <FaInstagram />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/rafael-lobo-plaza/">
+                    <FaLinkedin />
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
+        </div>
+        <div className="footer-section">
           <div className="footer-center">
-            <div className="content">
-              <p>Todos los derechos reservados | 2020 | Rafael Lobo Plaza</p>
+            <div className="footer-content">
+              <p>
+                Todos los derechos reservados | 2020 <br /> Rafael Lobo Plaza
+              </p>
             </div>
           </div>
+        </div>
+        <div className="footer-section">
           <div className="footer-right">
             <div className="sites">
-              <ul>Inicio</ul>
-              <ul>Acerca de</ul>
-              <ul>Portfolio</ul>
-              <ul>Contacto</ul>
-              <ul>Tecnologias</ul>
+              <ul>
+                <li>
+                  <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                  <Link to="/about">Acerca de</Link>
+                </li>
+                <li>
+                  <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contacto</Link>
+                </li>
+                <li>
+                  <Link to="/technologies">Tecnologias</Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
